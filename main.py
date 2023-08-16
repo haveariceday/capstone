330a5fc3e22de77a1aecb9ae4296bcf43f0289af
Hiromi Tucker <hiromidahomi@gmail.com>
2023-08-15 21:51:33 -0700

    fixed current_room and command problems by adding another parameter to command functions

--- main.py
+++ main.py
@@ -4,92 +4,93 @@
 player = {
     "name": "",
     "health": 100,
-    "inventory": []
+    "inventory": [],
+    "current_room": "lobby"
 }
 
 # Define game rooms and their descriptions
 rooms = {
-    "lodging": {
-        "description": "",
-        "exits": [],
-        "items": [""]
-    },
-    "theatre": {
-        "description": "",
-        "exits": [],
-        "items": [""]
-    },
-    "nightclub": {
-        "description": "",
-        "exits": [],
-        "items": [""]
-    },
-    "casino": {
-        "description": "",
-        "exits": [],
-        "items": [""]
-    },
-    "buffet": {
-        "description": "",
-        "exits": [],
-        "items": [""]
-    },
-    "crews break room": {
-        "description": "",
-        "exits": [],
-        "items": [""]
-    },
-    "steering room": {
-        "description": "",
-        "exits": [],
-        "items": [""]
-    },
-    "kitchen": {
-        "description": "",
-        "exits": [],
-        "items": [""]
-    },
-    "backstage": {
-        "description": "",
-        "exits": [],
-        "items": [""]
-    },
-    "engine room": {
-        "description": "",
-        "exits": [],
-        "items": [""]
-    }
-
-    # "lobby": {
-    #     "description": "You are in the lobby of an old mansion.",
-    #     "exits": ["kitchen", "living room", "hallway"],
-    #     "items": ["key"]
+    # "lodging": {
+    #     "description": "",
+    #     "exits": [casino,theatre,buffet],
+    #     "items": [""]
+    # },
+    # "theatre": {
+    #     "description": "",
+    #     "exits": [buffet, casino,backstage,lodging],
+    #     "items": [""]
+    # },
+    # "nightclub": {
+    #     "description": "",
+    #     "exits": [casino],
+    #     "items": [""]
+    # },
+    # "casino": {
+    #     "description": "",
+    #     "exits": [nightclub,theatre,lodging,buffet],
+    #     "items": [""]
+    # },
+    # "buffet": {
+    #     "description": "",
+    #     "exits": [nightclub,theatre,lodging,kitchen],
+    #     "items": [""]
+    # },
+    # "crews break room": {
+    #     "description": "",
+    #     "exits": [kitchen, engine room,backstage],
+    #     "items": [""]
+    # },
+    # "steering room": {
+    #     "description": "",
+    #     "exits": [engine room],
+    #     "items": [""]
     # },
     # "kitchen": {
-    #     "description": "You are in a dusty kitchen with old appliances.",
-    #     "exits": ["lobby"],
-    #     "items": ["knife"]
+    #     "description": "",
+    #     "exits": [buffet, crew break room],
+    #     "items": [""]
     # },
-    # "living room": {
-    #     "description": "You are in a grand living room with antique furniture.",
-    #     "exits": ["lobby", "dining room"],
-    #     "items": ["book"]
+    # "backstage": {
+    #     "description": "",
+    #     "exits": [theatre, crew break room],
+    #     "items": [""]
     # },
-    # "hallway": {
-    #     "description": "You are in a dimly lit hallway with many doors.",
-    #     "exits": ["lobby", "bedroom"],
-    #     "items": []
-    # },
-    # "dining room": {
-    #     "description": "You are in an elegant dining room with a large table.",
-    #     "exits": ["living room"],
-    #     "items": []
-    # },
-    # "bedroom": {
-    #     "description": "You are in a creepy bedroom with a creaky bed.",
-    #     "exits": ["hallway"],
-    #     "items": ["flashlight"]
+    # "engine room": {
+    #     "description": "",
+    #     "exits": [steering room, crew break room],
+    #     "items": [""]
     # }
+
+    "lobby": {
+        "description": "You are in the lobby of an old mansion.",
+        "exits": ["kitchen", "living room", "hallway"],
+        "items": ["key"]
+    },
+    "kitchen": {
+        "description": "You are in a dusty kitchen with old appliances.",
+        "exits": ["lobby"],
+        "items": ["knife"]
+    },
+    "living room": {
+        "description": "You are in a grand living room with antique furniture.",
+        "exits": ["lobby", "dining room"],
+        "items": ["book"]
+    },
+    "hallway": {
+        "description": "You are in a dimly lit hallway with many doors.",
+        "exits": ["lobby", "bedroom"],
+        "items": []
+    },
+    "dining room": {
+        "description": "You are in an elegant dining room with a large table.",
+        "exits": ["living room"],
+        "items": []
+    },
+    "bedroom": {
+        "description": "You are in a creepy bedroom with a creaky bed.",
+        "exits": ["hallway"],
+        "items": ["flashlight"]
+    }
 }
 
 # Define game actions
@@ -106,6 +107,7 @@ def display_status():
     print("Player: " + player["name"])
     print("Health: " + str(player["health"]))
     print("Inventory: " + str(player["inventory"]))
+    print("Current location: " + str(player["current_room"]))
 
 
 # Function to handle player input and perform actions
@@ -114,40 +116,40 @@ def handle_input(command):
         print("Game over. Thanks for playing!")
         return False
 
-    if command.startswith(actions["go"]):
+    if command.startswith(actions['go']):
         destination = command.split(actions["go"])[1].strip()
-        move_to_room(destination)
+        move_to_room(player["current_room"],destination)
     elif command.startswith(actions["look"]):
-        look_around()
+        look_around(player["current_room"])
     elif command.startswith(actions["take"]):
         item = command.split(actions["take"])[1].strip()
-        take_item(item)
+        take_item(player["current_room"],item)
     elif command.startswith(actions["use"]):
         item = command.split(actions["use"])[1].strip()
-        use_item(item)
+        use_item(player["current_room"],item)
     else:
         print("Unknown command.")
-
     return True
 
 
 # Function to move the player to a new room
-def move_to_room(destination):
+def move_to_room(current_room, destination):
     if destination in rooms[current_room]["exits"]:
-        current_room = destination
         print("Moving to " + destination + "...")
+        current_room = str(destination)
+        player["current_room"] = str(destination)
         print(rooms[current_room]["description"])
     else:
         print("You cannot go to " + destination + ".")
 
 
 # Function to look around the current room
-def look_around():
+def look_around(current_room):
     print(rooms[current_room]["description"])
 
 
 # Function to take an item and add it to the player's inventory
-def take_item(item):
+def take_item(current_room,item):
     if item in rooms[current_room]["items"]:
         player["inventory"].append(item)
         rooms[current_room]["items"].remove(item)
@@ -157,7 +159,7 @@ def take_item(item):
 
 
 # Function to use an item and perform a specific action
-def use_item(item):
+def use_item(current_room, item):
     if item == "key" and current_room == "bedroom":
         print("You have unlocked a hidden compartment!")
         # Additional game logic for unlocking the secret
@@ -173,12 +175,13 @@ def play_game():
     print("Welcome to the Escape Room Text Adventure Game!")
     player["name"] = input("Enter your name: ")
     print("Hello, " + player["name"] + "! Let's begin.")
-    current_room = "lobby"
+    #current_room = player["current_room"]
     display_status()
 
     game_running = True
     while game_running:
         command = input("Enter a command: ")
+        print("command print: ", command)
         game_running = handle_input(command)
 
 
